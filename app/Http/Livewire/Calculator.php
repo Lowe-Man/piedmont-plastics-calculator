d7d72f41706c1d6ba1178f5ff5d001c86425c1b7
Alex Lowe <loweman64@outlook.com>
2022-08-12 10:37:56 -0400

    docs: Add Comments to Model File

--- app/Http/Livewire/Calculator.php
+++ app/Http/Livewire/Calculator.php
@@ -11,6 +11,8 @@ class Calculator extends Component {
     public $width;
     public $thickness = .25;
 
+//  Make sure length is between 1-120 inclusive
+//  Make sure width is between 1-60 inclusive
     protected $rules = [
         'length' => [
             'required',
@@ -24,6 +26,7 @@ class Calculator extends Component {
 
     public function submit() {
         $this->validate();
+//       Remove lbs label if weight is equal to 0
         if ($this->material != 0) {
             $this->weight = round((($this->length * $this->width) / 144) * $this->thickness * $this->material * 5.2, 1);
             $this->weight = $this->weight . "<sub class='text-[24px]'>lbs</sub>";
@@ -32,12 +35,13 @@ class Calculator extends Component {
         }
     }
 
+//  Set form onload to invalid so that the submit button is grayed out by default
     public function mount() {
         $this->addError('length', 'The length field is invalid.');
         $this->addError('width', 'The width field is invalid.');
     }
 
-
+//  When a form field is updated run the rules above to make sure the inputs are valid
     public function updated($propertyName) {
         $this->validateOnly($propertyName);
     }
